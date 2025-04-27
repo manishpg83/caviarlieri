808455cb6b58b96b90b53d3972dc908e352d7a80
Manish Bhuva <manish.bhuvait@gmail.com>
2025-04-27 11:20:30 +0530

    debtors email and default product remove on add invendtory

--- app/Console/Commands/SendDebtorReminderMail.php
+++ app/Console/Commands/SendDebtorReminderMail.php
@@ -1,23 +1,23 @@
 <?php
- 
+
  namespace App\Console\Commands;
- 
+
  use Illuminate\Console\Command;
  use App\Models\OrderInvoice;
  use Carbon\Carbon;
  use App\Mail\DebtorReminderMail;
  use Illuminate\Support\Facades\Mail;
- 
+
  class SendDebtorReminderMail extends Command
  {
      protected $signature = 'debtors:remind';
- 
+
      protected $description = 'Send a reminder email to admin about unpaid invoices';
- 
+
      public function handle()
      {
          $threeWeeksAgo = Carbon::now()->subWeeks(3);
- 
+
          $debtors = OrderInvoice::with(['customer', 'createdBy', 'order'])
              ->whereHas('order', function($query) {
                  $query->whereHas('payments', function($q) {
@@ -26,13 +26,14 @@
              })
              ->where('created_at', '<=', $threeWeeksAgo)
              ->get();
- 
+
          $debtors->each(function ($invoice) {
              $invoice->overdue_days = (int)Carbon::parse($invoice->created_at)->diffInDays(now());
          });
- 
+
          if ($debtors->count() > 0) {
-             Mail::to('devanshu.briskbrain@gmail.com')->send(new DebtorReminderMail($debtors));
+             Mail::to('developer@predsolutions.com')->send(new DebtorReminderMail($debtors));
+             //Mail::to('developer@predsolutions.com')->send(new DebtorReminderMail($debtors));
              $this->info('Reminder email sent successfully.');
          } else {
              $this->info('No overdue invoices found.');
